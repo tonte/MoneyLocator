642e1fbcad38ee4b72359b75059ca48f11974afa
Tonte Owuso <tonte.owuso@dreamoval.com>
2018-06-23 10:11:33 +0000

    initial project creation

---
+++ Moneyloc/Moneyloc/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  Moneyloc
+//
+//  Created by Tonte Owuso on 6/23/18.
+//  Copyright © 2018 Qodehub. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainLabel.text = "Hello"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
